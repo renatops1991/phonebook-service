de86952026eae3d4d7e66c584d590041d577fce1
Renato Pereira <renatops1991@gmail.com>
2023-03-18 15:40:24 -0300

    chore: initial settings

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/protocols/**',
+    '!<rootDir>/src/**/domain/**',
+    '!<rootDir>/src/**/dtos/**',
+    '!<rootDir>/src/**/entities/**',
+    '!<rootDir>/src/**/types/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
