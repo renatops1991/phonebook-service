5e52b5a033ba50e6c6ca4043fe30722c82f747cd
Renato Pereira <renatops1991@gmail.com>
2023-03-19 17:29:37 -0300

    test: ensure return null if hasContact method returns true

--- tests/unit/data/use-cases/contact.spec.ts
+++ tests/unit/data/use-cases/contact.spec.ts
@@ -42,40 +42,39 @@ class ContactBuilderStub implements IContactBuilder {
 
 const contactRepositoryStub = new ContactRepositoryStub()
 const contactBuilderStub = new ContactBuilderStub()
+const sut = new Contact(contactRepositoryStub, contactBuilderStub)
 
 describe('Contact UseCase', () => {
   describe('Create Method', () => {
     it('Should call buildContact method of ContactBuilder class with correct values ', async () => {
-      const sut = new Contact(contactRepositoryStub, contactBuilderStub)
       const buildContactSpy = jest.spyOn(contactBuilderStub, 'buildContact')
       await sut.create(fixtureContact)
       expect(buildContactSpy).toHaveBeenCalledWith(fixtureContact)
     })
-    it('Should call create method of repository with correct values ', async () => {
-      const sut = new Contact(contactRepositoryStub, contactBuilderStub)
-      const createSpy = jest.spyOn(contactRepositoryStub, 'create')
-      await sut.create(fixtureContact)
-      expect(createSpy).toHaveBeenCalledWith(fixtureContact)
-    })
-    it('Should call loadByEmail method of repository with correct values ', async () => {
-      const sut = new Contact(contactRepositoryStub, contactBuilderStub)
+
+    it('Should call hasContact method of repository with correct values ', async () => {
       const hasContactSpy = jest.spyOn(contactRepositoryStub, 'hasContact')
       await sut.create(fixtureContact)
       expect(hasContactSpy).toHaveBeenCalledWith(fixtureContact.email)
     })
-    it('Should return null if loadByEmail method returns true', async () => {
-      const sut = new Contact(contactRepositoryStub, contactBuilderStub)
+    it('Should call create method of repository with correct values ', async () => {
+      const createSpy = jest.spyOn(contactRepositoryStub, 'create')
+      await sut.create(fixtureContact)
+      expect(createSpy).toHaveBeenCalledWith(fixtureContact)
+    })
+
+    it('Should return contact on success', async () => {
+      const expectResponse = await sut.create(fixtureContact)
+      expect(expectResponse).toEqual(fixtureContactOutput)
+    })
+
+    it('Should return null if hasContact method returns true', async () => {
       jest
         .spyOn(contactRepositoryStub, 'hasContact')
-        .mockReturnValue(Promise.resolve(true))
+        .mockReturnValue(new Promise(resolve => { resolve(true) }))
       await sut.create(fixtureContact)
       const expectedResponse = await sut.create(fixtureContact)
       expect(expectedResponse).toBeNull()
-    })
-    it('Should return contact on success', async () => {
-      const sut = new Contact(contactRepositoryStub, contactBuilderStub)
-      const expectResponse = await sut.create(fixtureContact)
-      expect(expectResponse).toEqual(fixtureContactOutput)
     })
   })
 })
