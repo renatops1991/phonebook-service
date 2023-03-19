3750a72ddc6b9fdeb9dc7b218a206986d4a19cbd
Renato Pereira <renatops1991@gmail.com>
2023-03-19 18:50:02 -0300

    feat: ensure connect with mongodb throught of mongoHelper

---
+++ src/infra/mongodb/mongo-helper.ts
@@ -0,0 +1,29 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  uri: null as unknown as string,
+
+  async connect (uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+    this.client = null
+  },
+
+  getCollection (collectionName: string): Collection {
+    return this.client.db().collection(collectionName)
+  },
+
+  map: (data: any): any => {
+    const { _id, ...collectionWithoutId } = data
+    return Object.assign({}, collectionWithoutId, { id: _id })
+  },
+
+  mapCollection: (dataCollection: any[]): any[] => {
+    return dataCollection.map(collection => MongoHelper.map(collection))
+  }
+}
