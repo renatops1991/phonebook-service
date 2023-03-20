3614b7548023fad3a882f93f1cc3ea020b2985e2
Renato Pereira <renatops1991@gmail.com>
2023-03-20 11:45:01 -0300

    feat: create a helper to instantiate custom error method

---
+++ src/presentation/helpers/http-protocols-helper.ts
@@ -0,0 +1,32 @@
+import { ServerError } from '../errors/server-error'
+import { HttpResponseType } from '../types/http-response-type'
+
+export const created = <T =any>(data: T): HttpResponseType => ({
+  statusCode: 201,
+  body: data
+})
+
+export const noContent = (): HttpResponseType => ({
+  statusCode: 204,
+  body: null
+})
+
+export const success = <T= any>(data: T): HttpResponseType => ({
+  statusCode: 200,
+  body: data
+})
+
+export const badRequest = (error: Error): HttpResponseType => ({
+  statusCode: 400,
+  body: error
+})
+
+export const forbidden = (error: Error): HttpResponseType => ({
+  statusCode: 403,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponseType => ({
+  statusCode: 500,
+  body: new ServerError(error.stack as string).serializeErrors()
+})
