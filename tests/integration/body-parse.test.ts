44011f6d7c94ec2c380cd607ac3859ffe03d7708
Renato Pereira <renatops1991@gmail.com>
2023-03-21 09:14:27 -0300

    test: ensure call body parse config to parse json

---
+++ tests/integration/body-parse.test.ts
@@ -0,0 +1,19 @@
+import { setupApp } from '@/main/config/app'
+import { Express } from 'express'
+import request from 'supertest'
+
+let app: Express
+describe('BodyParse Middleware', () => {
+  beforeAll(() => {
+    app = setupApp()
+  })
+  it('Should correct parse as json', async () => {
+    app.post('/body-parse', (request, response) => {
+      response.send(request.body)
+    })
+    await request(app)
+      .post('/body-parse')
+      .send({ name: 'foo' })
+      .expect({ name: 'foo' })
+  })
+})
