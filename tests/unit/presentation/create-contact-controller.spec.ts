3f478d759ccfbf794524153530688ec9e98843ed
Renato Pereira <renatops1991@gmail.com>
2023-03-22 15:42:15 -0300

    test: ensure call  validation composite class to validate fields

--- tests/unit/presentation/create-contact-controller.spec.ts
+++ tests/unit/presentation/create-contact-controller.spec.ts
@@ -4,16 +4,18 @@ import { CreateContactDto } from '@/main/dtos/create-contact.dto'
 import { CreateContact } from '@/presentation/controllers/create-contact'
 import { EmailInUseError } from '@/presentation/errors/email-in-use-error'
 import { ServerError } from '@/presentation/errors/server-error'
-import { forbidden, serverError } from '@/presentation/helpers/http-protocols-helper'
+import { badRequest, forbidden, serverError } from '@/presentation/helpers/http-protocols-helper'
 import { fixtureContact, fixtureContactOutput } from '@/tests/fixtures/fixturesContact'
+import { mockValidation } from '@/tests/mocks/mock-validate'
 
 class ContactStub implements IContact {
   async create (contactDto: CreateContactDto): Promise<ContactOutputDto | null> {
     return await new Promise(resolve => { resolve(fixtureContactOutput()) })
   }
 }
+const validationStub = mockValidation()
 const contactStub = new ContactStub()
-const sut = new CreateContact(contactStub)
+const sut = new CreateContact(contactStub, validationStub)
 describe('CreateContactController', () => {
   it('Should call create method of the Contact UseCase with correct values', async () => {
     const createContactSpy = jest
@@ -39,6 +41,23 @@ describe('CreateContactController', () => {
     const expectedResponse = await sut.handle(fixtureContact())
     expect(expectedResponse.statusCode).toBe(403)
     expect(expectedResponse).toEqual(forbidden((new EmailInUseError().serializeErrors())))
+  })
+
+  it('Should call validate method of the validation composite class with corrects values ', async () => {
+    const validationSpy = jest
+      .spyOn(validationStub, 'validate')
+    await sut.handle(fixtureContact())
+    expect(validationSpy).toHaveBeenCalledWith(fixtureContact())
+  })
+
+  it('Should return 400 error if validate method return error', async () => {
+    const contactDto = fixtureContact()
+    contactDto.email = 'foo@'
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new EmailInUseError().serializeErrors())
+    const expectedResponse = await sut.handle(contactDto)
+    expect(expectedResponse).toEqual(badRequest(new EmailInUseError().serializeErrors()))
   })
 
   it('Should return success if create method on succeeds', async () => {
