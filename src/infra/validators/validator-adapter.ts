98aca2146b76bb7cd8f0c4db20766664505a24b6
Renato Pereira <renatops1991@gmail.com>
2023-03-22 09:11:24 -0300

    feat: ensure validate phone number with isMobilePhone method

--- src/infra/validators/validator-adapter.ts
+++ src/infra/validators/validator-adapter.ts
@@ -2,7 +2,10 @@ import { IValidator } from '@/validation/protocols/validator'
 import validator from 'validator'
 
 export class ValidatorAdapter implements IValidator {
-  isValidPhoneNumber: (phone: string) => boolean
+  isValidPhoneNumber (phone: string): boolean {
+    return validator.isMobilePhone(phone, 'pt-BR')
+  }
+
   isValidEmail (email: string): boolean {
     return validator.isEmail(email)
   }
