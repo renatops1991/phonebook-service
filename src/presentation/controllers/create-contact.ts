9ef709ddc2ce02223ab12b610736c85305118346
Renato Pereira <renatops1991@gmail.com>
2023-03-22 14:22:52 -0300

    feat: ensure validate fields through the validation composite

--- src/presentation/controllers/create-contact.ts
+++ src/presentation/controllers/create-contact.ts
@@ -1,17 +1,24 @@
 import { IContact } from '@/domain/protocols/contact'
 import { CreateContactDto } from '@/main/dtos/create-contact.dto'
 import { EmailInUseError } from '../errors/email-in-use-error'
-import { created, forbidden, serverError } from '../helpers/http-protocols-helper'
+import { badRequest, created, forbidden, serverError } from '../helpers/http-protocols-helper'
 import { IController } from '../protocols/controller'
+import { IValidation } from '../protocols/validation'
 import { HttpResponseType } from '../types/http-response-type'
 
 export class CreateContact implements IController {
   constructor (
-    private readonly contact: IContact
+    private readonly contact: IContact,
+    private readonly validation: IValidation
   ) { }
 
   async handle (contactDto: CreateContactDto): Promise<HttpResponseType> {
     try {
+      const isError = this.validation.validate(contactDto)
+      if (isError) {
+        return badRequest(isError)
+      }
+
       const contact = await this.contact.create(contactDto)
       if (!contact) {
         return forbidden(new EmailInUseError().serializeErrors())
