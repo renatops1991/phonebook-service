3321c9caa1639a1efd385fc0c0357e4e2ee8b6ae
Renato Pereira <renatops1991@gmail.com>
2023-03-22 17:09:32 -0300

    feat: ensure required field validate

--- src/main/factories/validation-factory.ts
+++ src/main/factories/validation-factory.ts
@@ -2,10 +2,15 @@ import { ValidatorAdapter } from '@/infra/validators/validator-adapter'
 import { IValidation } from '@/presentation/protocols/validation'
 import { EmailValidate } from '@/validation/email-validate'
 import { PhoneNumberValidate } from '@/validation/phone-number-validate'
+import { RequiredField } from '@/validation/required-field'
 import { ValidationComposite } from '@/validation/validation-composite'
 
 export const makeValidationFactory = (): ValidationComposite => {
   const validations: IValidation[] = []
+
+  for (const field of ['name', 'email', 'address', 'phone']) {
+    validations.push(new RequiredField(field))
+  }
 
   validations.push(
     new EmailValidate('email', new ValidatorAdapter())
