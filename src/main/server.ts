ac63cfc8d7dc4512a426887ebc0efc4cf6abd0d4
Renato Pereira <renatops1991@gmail.com>
2023-03-22 11:08:31 -0300

    refactor: remove import unused of the server file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,3 @@
-import 'module-alias/register'
 import { MongoHelper } from '@/infra/mongodb/mongo-helper'
 import { Express } from 'express'
 import dotenv from 'dotenv'
@@ -7,7 +6,7 @@ dotenv.config()
 MongoHelper.connect(process.env.MONGODB_URI as string)
   .then(async () => {
     const { setupApp } = await import('./config/app')
-    const app: Express = setupApp()
+    const app: Express = await setupApp()
     app.listen(process.env.PORT,
       () => {
         console.log(`${String.fromCodePoint(0x1F525)} Server is running on port ${process.env.PORT}`)
