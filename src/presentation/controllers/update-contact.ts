96f5ec9fa5b7c2c961202879cac9ba116e2aa38d
Renato Pereira <renatops1991@gmail.com>
2023-04-04 23:20:45 -0300

    feat: ensure return badRequest error if the validation has error

--- src/presentation/controllers/update-contact.ts
+++ src/presentation/controllers/update-contact.ts
@@ -2,17 +2,24 @@ import { UpdateContactDto } from '@/main/dtos'
 import { IController } from '../protocols/controller'
 import { HttpResponseType } from '../types/http-response-type'
 import { IContact } from '@/domain/protocols/contact'
-import { serverError, success } from '../helpers/http-protocols-helper'
+import { badRequest, serverError, success } from '../helpers/http-protocols-helper'
+import { IValidation } from '../protocols/validation'
 
 export interface UpdateContactController extends UpdateContactDto { email: string }
 
 export class UpdateContact implements IController {
   constructor (
-    private readonly contact: IContact
+    private readonly contact: IContact,
+    private readonly validation: IValidation
   ) { }
 
   async handle (updateContactDto: UpdateContactController): Promise<HttpResponseType> {
     try {
+      const hasError = this.validation.validate(updateContactDto)
+      if (hasError) {
+        return badRequest(hasError)
+      }
+
       const contactUpdated = await this.contact.update(updateContactDto.email, updateContactDto)
       return success(contactUpdated)
     } catch (error) {
