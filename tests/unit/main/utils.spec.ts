ee5de9ce9281ba07140da63ba2696ba9e38aca54
Renato Pereira <renatops1991@gmail.com>
2023-04-11 10:30:19 -0300

    test: ensure create description to user based on temperature

--- tests/unit/main/utils.spec.ts
+++ tests/unit/main/utils.spec.ts
@@ -15,4 +15,42 @@ describe('Utils', () => {
       })
     })
   })
+
+  describe('makeDescriptionWeatherByTemperature', () => {
+    it('Should return "Convide seu contato para fazer uma caminhada" description by default', async () => {
+      const temperature = 24
+      const sut = utils.makeDescriptionWeatherByTemperature(temperature, undefined)
+      expect(sut).toEqual('Convide seu contato para fazer uma caminhada')
+    })
+
+    it('Should return "Ofereça um chocolate quente ao seu contato..." description it temperature is equal or less than 18', async () => {
+      const temperature = 18
+      const sut = utils.makeDescriptionWeatherByTemperature(temperature, undefined)
+      expect(sut).toEqual('Ofereça um chocolate quente ao seu contato...')
+    })
+
+    it('Should return "Convide seu contato para ir à praia com esse calor!" description if temperature greater than 30 and condition is equals 32', async () => {
+      const temperature = 31
+      const sut = utils.makeDescriptionWeatherByTemperature(temperature, '32')
+      expect(sut).toEqual('Convide seu contato para ir à praia com esse calor!')
+    })
+
+    it('Should return "Convide seu contato para tomar um sorvete" description if temperature greater than 30 and condition is equals 45', async () => {
+      const temperature = 31
+      const sut = utils.makeDescriptionWeatherByTemperature(temperature, '45')
+      expect(sut).toEqual('Convide seu contato para tomar um sorvete')
+    })
+
+    it('Should return "Convide seu contato para fazer alguma atividade ao livre" description if temperature greater than 18 and less than 30 and condition is equals 32', async () => {
+      const temperature = 24
+      const sut = utils.makeDescriptionWeatherByTemperature(temperature, '32')
+      expect(sut).toEqual('Convide seu contato para fazer alguma atividade ao livre')
+    })
+
+    it('Should return "Convide seu contato para ver um filme" description if temperature greater than 18 and less than 30 and condition is equals 45', async () => {
+      const temperature = 24
+      const sut = utils.makeDescriptionWeatherByTemperature(temperature, '45')
+      expect(sut).toEqual('Convide seu contato para ver um filme')
+    })
+  })
 })
