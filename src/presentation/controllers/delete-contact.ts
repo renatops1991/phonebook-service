91f64e86e11876bdecce5238d3b1ce50400d9d63
Renato Pereira <renatops1991@gmail.com>
2023-04-12 17:49:11 -0300

    feat: ensure call delete method of the usecase

---
+++ src/presentation/controllers/delete-contact.ts
@@ -0,0 +1,18 @@
+import { IContact } from '@/domain/protocols/contact'
+import { IController } from '../protocols/controller'
+import { HttpResponseType } from '../types/http-response-type'
+import { IValidation } from '../protocols/validation'
+
+export class DeleteContact implements IController {
+  constructor (
+    private readonly contact: IContact,
+    private readonly validation: IValidation
+  ) {}
+
+  async handle (email: string): Promise<HttpResponseType> {
+    await this.contact.delete(email)
+    return {
+      statusCode: 200
+    }
+  }
+}
