8e5b835c527f1b44414398fc76cbcf8dc7bf3260
Renato Pereira <renatops1991@gmail.com>
2023-04-13 16:19:16 -0300

    test: ensure return bad request error if validate method returns error

--- tests/unit/presentation/controllers/delete-contact.spec.ts
+++ tests/unit/presentation/controllers/delete-contact.spec.ts
@@ -2,6 +2,8 @@ import { DeleteContact } from '@/presentation/controllers/delete-contact'
 import { contactUseCaseStub } from '@/tests/unit/mocks/mock-contact'
 import { mockValidation } from '@/tests/unit/mocks/mock-validate'
 import { fixtureContact } from '../../fixtures/fixturesContact'
+import { EmailInUseError } from '@/presentation/errors/email-in-use-error'
+import { badRequest } from '@/presentation/helpers/http-protocols-helper'
 
 const validationStub = mockValidation()
 const contactStub = contactUseCaseStub()
@@ -20,5 +22,13 @@ describe('DeleteContactController', () => {
       .spyOn(validationStub, 'validate')
     await sut.handle(fixtureContact().email)
     expect(validateSpy).toHaveBeenCalledWith(fixtureContact().email)
+  })
+
+  it('Should return bad request error if validate method returns error', async () => {
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new EmailInUseError().serializeErrors())
+    const expectedResponse = await sut.handle(fixtureContact().email)
+    expect(expectedResponse).toEqual(badRequest(new EmailInUseError().serializeErrors()))
   })
 })
